130e6cc86839d38c6f4e43a5e5c84dd45105c31c
kerenschoss <kerensc@mta.ac.il>
2024-06-11 22:28:45 +0300

    add abstract

--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    internal class Vehicle
+    abstract class Vehicle
     {
         string modelName;
         string licenseNumber;
