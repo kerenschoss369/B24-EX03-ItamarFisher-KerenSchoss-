45e4faead18405e0ee7b43d7d2e226c63619ebc8
kerenschoss <kerensc@mta.ac.il>
2024-06-11 22:02:45 +0300

    Add project files.

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Vehicle
+    {
+        string modelName;
+        string licenseNumber;
+        float percentageOfEnergyLeft;
+        List<Wheel> WheelsList;
+        EnergySource energySource;
+    }
+}
---
+++ Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Wheel
+    {
+        string manufacturerName;
+        float currentAirPressure;
+        float MaxAirPressureDefinedByManufacturer;
+
+        void insreaseWheelAirPressure()
+        {
+
+        }
+    }
+}
